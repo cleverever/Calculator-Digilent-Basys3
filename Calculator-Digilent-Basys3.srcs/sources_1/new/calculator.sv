71fb9a326c0879596bc42b48b81123ea1745b10d
Everett Craw <2000craws@gmail.com>
2024-03-18 15:09:58 -0700

    Created accumulator for calculations

--- Calculator-Digilent-Basys3.srcs/sources_1/new/calculator.sv
+++ Calculator-Digilent-Basys3.srcs/sources_1/new/calculator.sv
@@ -40,7 +40,7 @@ endpackage
 
 module calculator
 (
-    input logic[13:0] sw,
+    input logic [13:0] sw,
     input logic btnu,
     input logic btnd,
     input logic btnc,
@@ -61,8 +61,12 @@ module calculator
 );
 
 logic [13:0] display_value;
+logic [13:0] alu_result;
+logic [13:0] accumulator;
+logic [13:0] user_value;
 
 CalcState state;
+ALU_Op op;
 
 state_machine STATE_MACHINE
 (
@@ -71,19 +75,22 @@ state_machine STATE_MACHINE
     .btnc(btnc),
     .btnl(btnl),
     .btnr(btnr),
-    .user_value(),
-    .alu_value(),
+    .user_value(user_value),
+    .alu_value(alu_result),
+    
     .state(state),
+    .op(op),
+    .accumulator(accumulator),
     .display_value(display_value)
 );
 
 
 alu ALU
 (
-    .op(),
-    .in0(),
-    .in1(),
-    .out()
+    .op(op),
+    .in0(accumulator),
+    .in1(user_value),
+    .out(alu_result)
 );
 
 seven_segment_display_controller DISPLAY_CONTROLLER
--- Calculator-Digilent-Basys3.srcs/sources_1/new/state_machine.sv
+++ Calculator-Digilent-Basys3.srcs/sources_1/new/state_machine.sv
@@ -28,14 +28,14 @@ module state_machine
     input logic btnc,
     input logic btnl,
     input logic btnr,
-    input logic[13:0] user_value,
-    input logic[13:0] alu_value,
+    input logic [13:0] user_value,
+    input logic [13:0] alu_value,
     
     output CalcState state,
     output ALU_Op op,
+    output logic [13:0] accumulator,
     output logic [13:0] display_value
 );
-
 logic btn_pressed;
 assign btn_pressed = btnu | btnd | btnc | btnl | btnr;
 
@@ -76,11 +76,13 @@ always_ff @(posedge btn_pressed) begin
             end
             else begin
                 state <= OP;
+                accumulator <= limit_value(user_value);
             end
         end
         OP : begin
             if(btnc) begin
                 state <= ANSWER;
+                accumulator <= limit_value(alu_value);
             end
             else begin
                 state <= OP;
@@ -89,6 +91,7 @@ always_ff @(posedge btn_pressed) begin
         ANSWER : begin
             if(btnc) begin
                 state <= CLEAR;
+                accumulator <= 0;
             end
             else begin
                 state <= OP;
@@ -96,4 +99,9 @@ always_ff @(posedge btn_pressed) begin
         end
     endcase
 end
+
+function logic [13:0] limit_value(input logic [13:0] value);
+    return value > 9999? 9999 : value;
+endfunction
+
 endmodule
